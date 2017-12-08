73a08d023b2a0b53bb9363e681f220883bc0d198
AndreasGeyer <andreas.geyer@geyer-net.de>
2017-12-08 11:34:29 +0100

    Request URLs angepasst

--- trace/driver/rest.js
+++ trace/driver/rest.js
@@ -1,13 +1,13 @@
 export class TraceClient {
     findAll() {
-        return fetch('http://localhost:8080/chat-core/graphql?query={allTraces{id clientthreadname serverthreadname message}}').then(res => {
+        return fetch('http://192.168.99.100:8080/chat-core/graphql?query={allTraces{id clientthreadname serverthreadname message}}').then(res => {
             return res.json();
         });
     }
 
     deleteAll() {
-    	const data = {query: "mutation={clearTraces}"}
-        return fetch("http://localhost:8080/chat-core/graphql", {
+    	const data = {query: "mutation{clearTrace}"}
+        return fetch("http://192.168.99.100:8080/chat-core/graphql", {
             method: 'POST',
             body: JSON.stringify(data)
         });
